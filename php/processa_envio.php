47209f2bff716c923157f78c1f251ecf86ff4e4d
RaphaelO-Melo <48573578+RaphaelO-Melo@users.noreply.github.com>
2020-07-28 14:28:53 -0300

    Using PHPMailer example

--- php/processa_envio.php
+++ php/processa_envio.php
@@ -20,7 +20,7 @@
 
     //print_r($mensagem); 
     if($mensagem->mensagemValida())
-        echo 'Mensagem válida';
+        enviaEmail();
     else
         echo 'Mensagem inválida';
 
@@ -31,33 +31,32 @@
             //Server settings
             $mail->SMTPDebug = 2;                                 // Enable verbose debug output
             $mail->isSMTP();                                      // Set mailer to use SMTP
-            $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+            $mail->Host = 'smtp.office365.com';                   // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
-            $mail->Username = 'user@example.com';                 // SMTP username
-            $mail->Password = 'secret';                           // SMTP password
-            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Username = 'raphael989898@outlook.com';        // SMTP username
+            $mail->Password = 'senha1234@';                       // SMTP password
+            $mail->SMTPSecure = 'STARTTLS';                       // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                                    // TCP port to connect to
 
             //Recipients
-            $mail->setFrom('from@example.com', 'Mailer');
-            $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-            $mail->addAddress('ellen@example.com');               // Name is optional
-            $mail->addReplyTo('info@example.com', 'Information');
-            $mail->addCC('cc@example.com');
-            $mail->addBCC('bcc@example.com');
+            $mail->setFrom('raphael989898@outlook.com', 'Web Completo - Remetente');
+            $mail->addAddress('mathewvicente@gmail.com', 'Web Completo - Destinatário');     // Add a recipient
+            //$mail->addReplyTo('raphael989898@outlook.com', 'Web Completo - Destinatário para resposta');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
 
             //Attachments
-            $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
             //Content
             $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = 'Abra esse email';
+            $mail->Body    = 'To mandadando email por php mulieke hehehe';
+            $mail->AltBody = 'To mandadando email por php mulieke hehehe';
 
             $mail->send();
-            echo 'Message has been sent';
+            echo 'Messagem enviada com sucesso';
         } catch (Exception $e) {
             echo 'Não foi possível enviar este email. Tente novamente mais tarde.';
             echo 'Detalhes do erro: ' . $mail->ErrorInfo;
