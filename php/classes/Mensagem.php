a24eafa55fe84255b716c58df4fd610780fae173
RaphaelO-Melo <48573578+RaphaelO-Melo@users.noreply.github.com>
2020-07-28 13:10:30 -0300

    Message class

---
+++ php/classes/Mensagem.php
@@ -0,0 +1,24 @@
+<?php
+
+    Class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($name){
+            return $this->$name;
+        }
+
+        public function __set($name, $value){
+            $this->$name = $value;
+        }
+
+        public function mensagemValida(){
+            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem))
+                return false;
+            else
+                return true;
+        }
+    }
+
+?>
--- php/processa_envio.php
+++ php/processa_envio.php
@@ -1,5 +1,17 @@
 <?php
 
-    print_r($_POST);
+    //Importações
+    include_once 'classes/Mensagem.php';
+    
+    $mensagem = new Mensagem();
+    
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
 
+    //print_r($mensagem); 
+    if($mensagem->mensagemValida())
+        echo 'Mensagem válida';
+    else
+        echo 'Mensagem inválida';
 ?>
